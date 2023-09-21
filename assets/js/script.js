1fc0e58012e4b0eba6434677b90f4d81fd7d9e77
callTx <danielfdeus@gmail.com>
2023-09-21 14:09:51 -0300

    Remove modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@
 // const modalCloseBtn = document.querySelector('[data-modal-close]');
 // const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
-// // modal function
+// modal function
 // const modalCloseFunc = function () { modal.classList.add('closed') }
 
-// // modal eventListener
+// modal eventListener
 // modalCloseOverlay.addEventListener('click', modalCloseFunc);
 // modalCloseBtn.addEventListener('click', modalCloseFunc);
 
@@ -17,13 +17,13 @@
 
 
 // notification toast variables
-const notificationToast = document.querySelector('[data-toast]');
-const toastCloseBtn = document.querySelector('[data-toast-close]');
+// const notificationToast = document.querySelector('[data-toast]');
+// const toastCloseBtn = document.querySelector('[data-toast-close]');
 
 // notification toast eventListener
-toastCloseBtn.addEventListener('click', function () {
-  notificationToast.classList.add('closed');
-});
+// toastCloseBtn.addEventListener('click', function () {
+//   notificationToast.classList.add('closed');
+// });
 
 
 
