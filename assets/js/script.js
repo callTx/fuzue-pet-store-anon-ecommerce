49c79f8024efda891dee79111b2b1691eadcf146
callTx <danielfdeus@gmail.com>
2023-09-21 14:04:04 -0300

    Remove listeners do modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // modal variables
-const modal = document.querySelector('[data-modal]');
-const modalCloseBtn = document.querySelector('[data-modal-close]');
-const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+// const modal = document.querySelector('[data-modal]');
+// const modalCloseBtn = document.querySelector('[data-modal-close]');
+// const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
-// modal function
-const modalCloseFunc = function () { modal.classList.add('closed') }
+// // modal function
+// const modalCloseFunc = function () { modal.classList.add('closed') }
 
-// modal eventListener
-modalCloseOverlay.addEventListener('click', modalCloseFunc);
-modalCloseBtn.addEventListener('click', modalCloseFunc);
+// // modal eventListener
+// modalCloseOverlay.addEventListener('click', modalCloseFunc);
+// modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 
 
