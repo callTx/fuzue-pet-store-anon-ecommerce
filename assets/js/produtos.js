5467e2a64384551e0bdf264dcf42d50dd5addf5c
callTx <danielfdeus@gmail.com>
2023-09-23 14:09:38 -0300

    Faz append da tag showcase a tag productGridDiv

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -15,6 +15,28 @@ const products = [
   }
 ];
 
+const productContainer = document.createElement("div");
+productContainer.classList.add("product-container");
+
+const containerDiv = document.createElement("div");
+containerDiv.classList.add("container");
+
+const productBoxDiv = document.createElement("div");
+productBoxDiv.classList.add("product-box");
+
+const productMainDiv = document.createElement("div");
+productMainDiv.classList.add("product-main");
+
+const titleH2 = document.createElement("h2");
+titleH2.classList.add("title");
+titleH2.textContent = "Petiscos Naturais";
+
+const productGridDiv = document.createElement("div");
+productGridDiv.classList.add("product-grid");
+
+
+
+
 function generateProductHTML(product) {
   return `
   <div class="product-container">
@@ -55,11 +77,22 @@ function generateProductHTML(product) {
     `;
 }
 
-const productContainer = document.getElementById("productContainer");
+const showcase = document.getElementById("showcase");
 products.forEach((product) => {
   const productHTML = generateProductHTML(product);
-  productContainer.insertAdjacentHTML("beforeend", productHTML);
+  showcase.insertAdjacentHTML("beforeend", productHTML);
 });
+productGridDiv.appendChild(showcase);
+
+productMainDiv.appendChild(titleH2);
+productMainDiv.appendChild(productGridDiv);
+
+productBoxDiv.appendChild(productMainDiv);
+
+containerDiv.appendChild(productBoxDiv);
+
+productContainer.appendChild(containerDiv);
+
 
 // Add the CSS link dynamically
 const head = document.head || document.getElementsByTagName("head")[0];
@@ -67,3 +100,5 @@ const cssLink = document.createElement("link");
 cssLink.rel = "stylesheet";
 cssLink.href = "./assets/css/style-prefix.css";
 head.appendChild(cssLink);
+
+document.body.appendChild(productContainer);
