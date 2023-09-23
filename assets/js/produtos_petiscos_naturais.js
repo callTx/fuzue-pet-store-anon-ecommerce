340939aef43b1f39cc06a665ed3c5bfedfe770b4
callTx <danielfdeus@gmail.com>
2023-09-23 14:58:51 -0300

    Add mais produtos ao lista json de petiscos

--- assets/js/produtos_petiscos_naturais.js
+++ assets/js/produtos_petiscos_naturais.js
@@ -4,15 +4,74 @@ const products = [
     altText: "Petisco de Cenoura",
     category: "Petisco de Cenoura",
     title: "Petiscos Assados The Bull",
-    price: "R$18,90"
+    price: "R$18,90",
+    pageSrc: "#"
   },
   {
     imgSrc: "./assets/images/products/snack-de-frango.jpg",
     altText: "Snack de Frango",
     category: "Snack de Frango",
     title: "Snack de Frango The Bull",
-    price: "R$28,90"
-  }
+    price: "R$28,90",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/Snack-file-suino.jpg",
+    altText: "Snack Filé Suíno",
+    category: "Snack Filé Suíno",
+    title: "Snack Filé Suíno The Bull",
+    price: "R$ 30,00",
+    pageSrc: "#"
+  },
+  
+  {
+    imgSrc: "./assets/images/products/snack-de-frango.jpg",
+    altText: "Snack de Frango",
+    category: "Snack de Frango",
+    title: "Snack de Frango The Bull",
+    price: "R$ 28,00",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/snack-de-figado.jpg",
+    altText: "Snack de Fígado",
+    category: "Snack de Fígado",
+    title: "Snack de Fígado The Bull",
+    price: "R$ 28,00",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/snack-figado-de-avestruz.jpg",
+    altText: "Snack de Fígado de Avestruz",
+    category: "Snack de Fígado de Avestruz",
+    title: "Snack de Fígado de Avestruz The Bull",
+    price: "R$ 36,00",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/snack-de-manjuba.jpg",
+    altText: "Snack de Manjuba",
+    category: "Snack de Manjuba",
+    title: "Snack de Manjuba The Bull",
+    price: "R$ 30,00",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/snack-de-buchino.jpg",
+    altText: "Snack de Buchinho",
+    category: "Snack de Buchinho",
+    title: "Snack de Buchinho The Bull",
+    price: "R$ 28,00",
+    pageSrc: "#"
+  },
+  {
+    imgSrc: "./assets/images/products/biscoito-de-banana-e-aveia.jpg",
+    altText: "Biscoito de Banana e Aveia",
+    category: "Biscoito de Banana e Aveia",
+    title: "Biscoito de Banana e Aveia The Bull",
+    price: "R$ 25,00",
+    pageSrc: "#"
+  },
 ];
 
 const productContainer = document.createElement("div");
@@ -44,8 +103,12 @@ function generateProductHTML(product) {
   return `
     <div class="showcase">
       <div class="showcase-banner">
+        <a href="${product.pageSrc}">
         <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img default">
+        <a/>
+        <a href="${product.pageSrc}">
         <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img hover">
+        <a/>
       </div>
       <div class="showcase-content">
         <a href="#" class="showcase-category">${product.category}</a>
