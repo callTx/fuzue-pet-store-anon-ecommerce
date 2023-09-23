912e0ecd8183c8160a266b5c976833af8c319aed
callTx <danielfdeus@gmail.com>
2023-09-23 13:28:36 -0300

    Add nova refatoracao

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,19 +1,43 @@
 const products = [
-    {
-      imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
-      altText: "Petisco de Cenoura",
-      category: "Petisco de Cenoura",
-      title: "Petiscos Assados The Bull",
-      rating: 5,
-      price: "R$25,00"
-    },
-    {
-        imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
-        altText: "Petisco de Cenoura",
-        category: "Petisco de Cenoura",
-        title: "Petiscos Assados The Bull",
-        rating: 5,
-        price: "R$25,00"
-      },
-  ];
-  
+  {
+    imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
+    altText: "Petisco de Cenoura",
+    category: "Petisco de Cenoura",
+    title: "Petiscos Assados The Bull",
+    rating: 5,
+    price: "R$25,00"
+  },
+  {
+    imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
+    altText: "Petisco de Cenoura",
+    category: "Petisco de Cenoura",
+    title: "Petiscos Assados The Bull",
+    rating: 5,
+    price: "R$25,00"
+  },
+  // Add more products here as needed
+];
+
+function generateProductShowcase(products) {
+  return products.map(product => `
+    <div class="showcase">
+      <div class="showcase-banner">
+        <a href="https://fuzue-pet-store-anon-ecommerce.vercel.app/petiscos_petisco-de-cenoura.html">
+          <img src="${product.imageSrc}" alt="${product.altText}" width="300" class="product-img default">
+          <img src="${product.imageSrc}" alt="${product.altText}" width="300" class="product-img hover">
+        </a>
+      </div>
+      <div class="showcase-content">
+        <a href="https://fuzue-pet-store-anon-ecommerce.vercel.app/petiscos_petisco-de-cenoura.html" class="showcase-category">${product.category}</a>
+        <a href="https://fuzue-pet-store-anon-ecommerce.vercel.app/petiscos_petisco-de-cenoura.html">
+          <h3 class="showcase-title">${product.title}</h3>
+        </a>
+        <div class="showcase-rating">
+          ${Array(product.rating).fill('<ion-icon name="star"></ion-icon>').join('')}
+        </div>
+        <div class="price-box">
+          <p class="price">${product.price}</p>
+        </div>
+      </div>
+    </div>`).join('');
+}
