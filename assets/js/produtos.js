9f3eeb2a7d86014a287ae34be4a5625020c37a7c
callTx <danielfdeus@gmail.com>
2023-09-23 13:51:23 -0300

    Add nova refatoracao

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,50 +1,69 @@
-function generateProductShowcase(product) {
+const products = [
+  {
+    imgSrc: "./assets/images/products/petisco-de-cenoura.jpg",
+    altText: "Petisco de Cenoura",
+    category: "Petisco de Cenoura",
+    title: "Petiscos Assados The Bull",
+    price: "R$18,90"
+  },
+  {
+    imgSrc: "./assets/images/products/snack-de-frango.jpg",
+    altText: "Snack de Frango",
+    category: "Snack de Frango",
+    title: "Snack de Frango The Bull",
+    price: "R$28,90"
+  }
+];
+
+function generateProductHTML(product) {
   return `
-    <div class="product-container">
+  <div class="product-container">
+
       <div class="container">
+
         <div class="product-box">
           <div class="product-main">
-            <h2 class="title">${product.title}</h2>
+
+            <h2 class="title">Petiscos Naturais</h2>
+
             <div class="product-grid">
-              <div class="showcase">
-                <div class="showcase-banner">
-                  <a href="${product.link}">
-                    <img src="${product.imageSrc}" alt="${product.altText}" width="300" class="product-img default">
-                    <img src="${product.imageSrc}" alt="${product.altText}" width="300" class="product-img hover">
-                  </a>
-                </div>
-                <div class="showcase-content">
-                  <a href="${product.link}" class="showcase-category">${product.category}</a>
-                  <a href="${product.link}">
-                    <h3 class="showcase-title">${product.title}</h3>
-                  </a>
-                  <div class="showcase-rating">
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
+                <div class="showcase">
+                  <div class="showcase-banner">
+                    <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img default">
+                    <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img hover">
                   </div>
-                  <div class="price-box">
-                    <p class="price">${product.price}</p>
+                  <div class="showcase-content">
+                    <a href="#" class="showcase-category">${product.category}</a>
+                    <a href="#"><h3 class="showcase-title">${product.title}</h3></a>
+                    <div class="showcase-rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                    <div class="price-box">
+                      <p class="price">${product.price}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>`;
+    </div>
+    `;
 }
 
-const productInfo = {
-  title: "Petiscos Assados The Bull",
-  category: "Petisco de Cenoura",
-  link: "https://fuzue-pet-store-anon-ecommerce.vercel.app/petiscos_petisco-de-cenoura.html",
-  imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
-  altText: "Petisco de Cenoura",
-  price: "R$25,00"
-};
+const productContainer = document.getElementById("productContainer");
+products.forEach((product) => {
+  const productHTML = generateProductHTML(product);
+  productContainer.insertAdjacentHTML("beforeend", productHTML);
+});
 
-const productShowcaseHTML = generateProductShowcase(productInfo);
-console.log(productShowcaseHTML);  // You can modify this to insert the HTML where needed in your application
+// Add the CSS link dynamically
+const head = document.head || document.getElementsByTagName("head")[0];
+const cssLink = document.createElement("link");
+cssLink.rel = "stylesheet";
+cssLink.href = "./assets/css/style-prefix.css";
+head.appendChild(cssLink);
