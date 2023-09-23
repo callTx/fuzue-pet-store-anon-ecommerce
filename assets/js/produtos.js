20b348fa09d38391c76ccffaf62abb8117228ba5
callTx <danielfdeus@gmail.com>
2023-09-23 14:42:35 -0300

    Atualiza script produto para corrigir exibicao dos produtos em forma de grid

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -34,43 +34,37 @@ titleH2.textContent = "Petiscos Naturais";
 const productGridDiv = document.createElement("div");
 productGridDiv.classList.add("product-grid");
 
-
-
+// Iterate through products and generate HTML for each product
+products.forEach((product) => {
+  const productHTML = generateProductHTML(product);
+  productGridDiv.innerHTML += productHTML;
+});
 
 function generateProductHTML(product) {
   return `
-  
-                <div class="showcase">
-                  <div class="showcase-banner">
-                    <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img default">
-                    <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img hover">
-                  </div>
-                  <div class="showcase-content">
-                    <a href="#" class="showcase-category">${product.category}</a>
-                    <a href="#"><h3 class="showcase-title">${product.title}</h3></a>
-                    <div class="showcase-rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                    <div class="price-box">
-                      <p class="price">${product.price}</p>
-                    </div>
-                  </div>
-                </div>
-            
-    `;
+    <div class="showcase">
+      <div class="showcase-banner">
+        <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img default">
+        <img src="${product.imgSrc}" alt="${product.altText}" width="300" class="product-img hover">
+      </div>
+      <div class="showcase-content">
+        <a href="#" class="showcase-category">${product.category}</a>
+        <a href="#"><h3 class="showcase-title">${product.title}</h3></a>
+        <div class="showcase-rating">
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+        </div>
+        <div class="price-box">
+          <p class="price">${product.price}</p>
+        </div>
+      </div>
+    </div>`;
 }
 
-const showcase = document.getElementById("showcase");
-products.forEach((product) => {
-  const productHTML = generateProductHTML(product);
-  showcase.insertAdjacentHTML("beforeend", productHTML);
-});
-productGridDiv.appendChild(showcase);
-
+// Append the generated product grid to the main product grid div
 productMainDiv.appendChild(titleH2);
 productMainDiv.appendChild(productGridDiv);
 
@@ -79,7 +73,6 @@ productBoxDiv.appendChild(productMainDiv);
 containerDiv.appendChild(productBoxDiv);
 
 productContainer.appendChild(containerDiv);
-
 
 // Add the CSS link dynamically
 const head = document.head || document.getElementsByTagName("head")[0];
