716b7483b0c36dbea8d0655607e7baafeb40591c
callTx <danielfdeus@gmail.com>
2023-09-23 13:26:13 -0300

    Add js produtos e faz nova refatoracao

---
+++ assets/js/produtos.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+      imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
+      altText: "Petisco de Cenoura",
+      category: "Petisco de Cenoura",
+      title: "Petiscos Assados The Bull",
+      rating: 5,
+      price: "R$25,00"
+    },
+    {
+        imageSrc: "./assets/images/products/petisco-de-cenoura.jpg",
+        altText: "Petisco de Cenoura",
+        category: "Petisco de Cenoura",
+        title: "Petiscos Assados The Bull",
+        rating: 5,
+        price: "R$25,00"
+      },
+  ];
+  
