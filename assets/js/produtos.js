da7e64e7589f6f35a3d235e4318ee5c9452be6cc
callTx <danielfdeus@gmail.com>
2023-09-26 13:44:05 -0300

    Add null a filteredProducts

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -392,7 +392,7 @@ const filtro = getParameterByName('filtro');
 
 
 //const category = document.getElementById('showcase').dataset.category;
-const filteredProducts = any;
+const filteredProducts = null;
 if (category != null){
     var categoriaFormatada = formatString(category);
 
